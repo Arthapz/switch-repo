bfbaeca224e0c4c364b5d79f96d99af6740f316e
Arthur LAURENT <arthur.laurent4@gmail.com>
2023-05-24 20:31:38 +0200

    update packages

--- packages/s/switch-llvm/xmake.lua
+++ packages/s/switch-llvm/xmake.lua
@@ -3,30 +3,40 @@ package("switch-llvm")
     set_homepage("https://llvm.org/")
     set_description("The LLVM Compiler Infrastructure, targetting Nintendo switch environment")
 
-    set_urls("https://github.com/llvm/llvm-project.git")
-    add_versions("20230517", "6f7a3b078191a925546ea3fead2e9cf0efdd9257")
+    set_urls("https://github.com/llvm/llvm-project.git", {branch = "main"})
+    add_versions("20230524-1", "1894c741b0cadb8daf3b25d3e4896d14ecc7f35a")
 
     add_deps("cmake", {kind = "binary", host = true})
+    add_deps("ninja", {kind = "binary", host = true})
     add_deps("python 3.x", {kind = "binary", host = true})
+    add_deps("switch-support-files")
 
-    add_configs("use_ninja", {description = "Use ninja as cmake backend", default = true, type = "boolean"})
+    add_configs("lldb", {description = "Build and install lldb", default = false, type = "boolean"})
 
-    on_install("windows", "macos", "linux", function(package)
-        local opt
+    on_load(function(package)
+        if is_host("windows") then
+            package:add("linkdirs", path.join(package:installdir("lib"), "clang", "17", "lib", is_host("windows") and "windows" or "aarch64-none-elf"))
+        end
 
-        if package:config("use_ninja") then
-            opt = opt or {}
-            opt.cmake_generator = "Ninja"
-            package:add("deps", "ninja", {binary = true, host = true})
+        package:add("links", "clang_rt.builtins-aarch64")
+    end)
+
+    on_install("@windows", "@macos", "@linux", function(package)
+        local opt = {}
+        opt.cmake_generator = "Ninja"
+
+        local projects = "clang;lld"
+        if package:config("lldb") then
+            projects = projects .. ";lldb"
         end
 
         os.cd("llvm")
         import("package.tools.cmake").install(package, {
             "-DCMAKE_BUILD_TYPE=Release",
-            "-DLLVM_ENABLE_PROJECTS=clang;lld;lldb",
+            "-DLLVM_ENABLE_PROJECTS=" .. projects,
             "-DLLVM_ENABLE_RUNTIMES=compiler-rt",
             "-DLLVM_TARGETS_TO_BUILD=AArch64",
-            "-DLLVM_DEFAULT_TARGET_TRIPLE=aarch64-none-elf",
+            "-DLLVM_DEFAULT_TARGET_TRIPLE=aarch64-nxos-elf",
             "-DLLVM_INSTALL_UTILS=ON",
             "-DLLVM_ENABLE_PIC=ON",
             "-DLLVM_ENABLE_MODULES=ON",
@@ -46,4 +56,9 @@ package("switch-llvm")
             "-DCOMPILER_RT_BUILD_LIBFUZZER=OFF",
             "-DCOMPILER_RT_BUILD_PROFILE=OFF"
         }, opt)
+    end)
+
+    on_test(function (package)
+        os.vrun("llvm-config --version")
+        os.vrun("clang --version")
     end)
--- packages/s/switch-newlib/xmake.lua
+++ packages/s/switch-newlib/xmake.lua
@@ -1,6 +1,8 @@
 package("switch-newlib")
     add_urls("https://github.com/Arthapz/newlib.git", {branch = "devkitPro"})
-    add_versions("20230519", "aaa8c961627e09dc4de8b73063c3f8ef5ff0e6ad")
+    add_versions("20230526-6", "76a553cd2909fe9bf2fac22c5d789ecbc681a9d5")
+
+    add_defines("_GNU_SOURCE=1", "_LIBC", "_NEWLIB_VERSION=4.3.0", "__NEWLIB__=4")
 
     on_install("switch", function(package)
         import("package.tools.xmake").install(package)
